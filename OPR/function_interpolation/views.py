ae7aa20b41575fca0eba553f8689da5f3145ac40
golovach_v <4231267@gmail.com>
2022-10-11 14:48:24 +0300

    version 0.79

--- OPR/function_interpolation/views.py
+++ OPR/function_interpolation/views.py
@@ -1,8 +1,9 @@
 import mpld3
+from bokeh.embed import components
 from django.shortcuts import render
 
-# Create your views here.
 
+# Create your views here.
 
 def index(request):
     from scipy.interpolate import CubicSpline
@@ -10,14 +11,19 @@ def index(request):
     import matplotlib.pyplot as plt
 
     plt.style.use('seaborn-poster')
-    x = [0, 1, 2,6]
-    y = [1, 3, 2,3]
+    if request.POST:
 
+        x = [int(val) for val in request.POST.getlist('x')]
+        y = [int(val) for val in request.POST.getlist('y')]
+
+    else:
+        x = [0, 1, 2, 6]
+        y = [1, 3, 2, 3]
     # use bc_type = 'natural' adds the constraints as we described above
     f = CubicSpline(x, y, bc_type='natural')
     x_new = np.linspace(0, 6, 100)
     y_new = f(x_new)
-
+    # bokeh try
     from bokeh.plotting import figure, show
 
     # prepare some data
@@ -29,17 +35,26 @@ def index(request):
     p.line(x_new, y_new, legend_label="Temp.", line_width=2)
 
     # show the results
-    show(p)
+    script, div = components(p)
 
-    fig = plt.figure(figsize=(10, 8))
-    plt.plot(x_new, y_new, 'b')
-    plt.plot(x, y, 'ro')
+    fig, ax1 = plt.subplots(figsize=(10, 6))
+    ax1.plot(x_new, y_new, 'b')
+    ax1.plot(x, y, 'ro')
     plt.title('Cubic Spline Interpolation')
     plt.xlabel('x')
     plt.ylabel('y')
-    plt.show()
+
+    labels = [f'x: {a}<br>y: {b}' for a, b in zip(x, y)]
+
+    position = mpld3.plugins.MousePosition()
+    mpld3.plugins.connect(fig, position)
+
+    # tooltip = mpld3.plugins.PointLabelTooltip(fig, labels=labels)
+    tooltip = mpld3.plugins.PointHTMLTooltip(fig, labels=labels)
+    mpld3.plugins.connect(fig, tooltip)
+
+    # print(labels)
+    # plt.show()
     html_str = mpld3.fig_to_html(fig)
-    Html_file = open("index.html", "w")
-    Html_file.write(html_str)
-    Html_file.close()
-    return render(request, "index.html", {"html_str": html_str})
+
+    return render(request, "index.html", {"html_str": html_str,"xy":zip(x_new,y_new),'script': script, 'div': div})
